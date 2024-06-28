785f8dbe4a00c156b7c24e4cb0c138b68d438c80
최진호 <g1063114@naver.com>
2024-06-28 17:11:44 +0900

    init : test-code-architecture project

---
+++ src/main/kotlin/kr/co/testcode/TestcodeApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.testcode
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestcodeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestcodeApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/testcode/TestcodeApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.testcode
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestcodeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
